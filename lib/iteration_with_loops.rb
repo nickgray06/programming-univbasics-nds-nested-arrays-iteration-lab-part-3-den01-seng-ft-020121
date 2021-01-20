3bdec37e3381e6b9d906a42bc69429578a2bfc6e
Nick Gray <nickgray87@gmail.com>
2021-01-20 21:25:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,10 +3,10 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   string_array = []
   row_index = 0
-  while row_index < src.count do
+  while row_index < src.length do
     element_index = 0
   end
-  while element_index < src[row_index].count do
+  while element_index < src[row_index].length do
     if src[row_index][element_index].class == String
       string_only = src[row_index][element_index]
     end
